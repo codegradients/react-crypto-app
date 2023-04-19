46f9a4c18f74b0ef0f904be8dba46fa85fab56ba
{shah0221} <{my>
2023-04-19 15:45:21 +0500

    initial commit

---
+++ src/components/ExchangesCard.jsx
@@ -0,0 +1,34 @@
+import { Heading, Image, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+const ExchangesCard = ({ name, img, rank, url }) => (
+  <a href={url} target="blank">
+    <VStack
+      w={"52"}
+      shadow={"lg"}
+      p={"8"}
+      borderRadius={"lg"}
+      transition={"all 0.3s"}
+      m={"4"}
+      css={{
+        "&:hover": {
+          transform: "scale(1.1)",
+        },
+      }}
+    >
+      <Image
+        src={img}
+        h={"10"}
+        w={"10"}
+        objectFit={"contain"}
+        alt={"Exchange"}
+      />
+      <Heading size={"md"} noOfLines={1}>
+        {rank}
+      </Heading>
+      <Text noOfLines={1}>{name}</Text>
+    </VStack>
+  </a>
+);
+
+export default ExchangesCard;
